e790cde1e7244577ec264dc8af1a1323c23be1bd
congkc <kieuchicong99@gmail.com>
2022-08-20 23:15:06 +0700

    add template

--- android/app/src/main/java/com/scheduleapp/MainActivity.java
+++ android/app/src/main/java/com/scheduleapp/MainActivity.java
@@ -1,5 +1,5 @@
 package com.scheduleapp;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -13,6 +13,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "ScheduleApp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
@@ -44,5 +49,7 @@ public class MainActivity extends ReactActivity {
       // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
       return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     }
+
+   
   }
 }
